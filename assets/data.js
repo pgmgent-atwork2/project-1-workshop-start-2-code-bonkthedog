2a4b1a88c254b6894442bd5ab96a1bed872a7deb
pgm-eliabello <eliabello@student.arteveldehs.be>
2024-04-18 15:29:43 +0200

    patch

---
+++ assets/data.js
@@ -0,0 +1,10 @@
+const dogImages = [
+    "img/daggoe-cry.jpg",
+    "img/daggoe-gamer.jpg",
+    "img/daggoe-mac.jpg",
+    "img/daggoe-mc.jpg",
+    "img/daggoe-mlg.jpg",
+    "img/daggoe-strong.jpg",
+    "img/daggoe.jpg",
+]
+
