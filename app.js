dd5a73e3a64e1c1ea79fa815a1dd5dc3deba7e9e
pgm-eliabello <eliabello@student.arteveldehs.be>
2024-04-18 16:19:10 +0200

    patch

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ let imageIndex = 0;
 document.getElementById("toggleImages").addEventListener("click", function() {
     const images = document.querySelectorAll(".imgHide");
     if (imageIndex < images.length) {
-        images[imageIndex].classList.remove("dog[imgageIndex]");
+        images[imageIndex].classList.remove("dog[imageIndex]");
         images[imageIndex].classList.add("imgShow");
         imageIndex++;
+    }
+    else {
+        document.getElementById("toggleImages").textContent = "Max reached";
     }
 });
