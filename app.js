b6d4b48a0f6decf42bd6978390d0976662f73490
pgm-eliabello <eliabello@student.arteveldehs.be>
2024-04-19 10:28:00 +0200

    patch

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 let imageIndex = 0;
 let counterValue = 0;
 
-document.getElementById("toggleImages").addEventListener("click", function() {
-    const images = document.querySelectorAll(".imgHide");
-    if (imageIndex < images.length) {
-        images[imageIndex].classList.remove("dog[imageIndex]");
-        images[imageIndex].classList.add("imgShow");
-        imageIndex++;
-    }
-    else {
-        document.getElementById("toggleImages").textContent = "Max reached";
+document.getElementById("dogBat").addEventListener("click", function() {
+    if (counterValue >= 50) {
+        const images = document.querySelectorAll(".imgHide");
+        if (imageIndex < images.length) {
+            images[imageIndex].classList.remove("dog[imageIndex]");
+            images[imageIndex].classList.add("imgShow");
+            imageIndex++;
+        }
+        else {
+            document.getElementById("dogBat").textContent = "Max reached";
+        }
+        counterValue -= 50;
+        document.querySelector(".clickCounter").textContent = counterValue;
+    } else {
+        alert("You don't have enough points");
     }
 });
 
@@ -22,3 +28,4 @@ setInterval(function() {
     counterValue += imageIndex ;
     document.querySelector(".clickCounter").textContent = counterValue;
 }, 1000);
+
